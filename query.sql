66f65a6228bb1b12ab19e96669d99cd00030db82
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-11-24 18:49:51 -0300

    Iniciado projeto para implementar o banco de dados PostgreSQL localmente

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 -- Exemplo de tabela 
 
 -- CREATE TABLE friends (
---     id SERIAL PRYMARY KEY,
+--     id SERIAL PRYMARY KEY,  // SERIAL implementa o id automatico e na sequencia correta
 --     name VARCHAR(50),
 --     age INT,
 --     is_cool BOOLEAN  //verdadeiro ou falso
