8642aee318c1791eee7d536862fe4e9d7ca5b503
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-12-01 19:39:23 -0300

    Aprendido o LIKE no postgreSQL

--- query.sql
+++ query.sql
@@ -6,3 +6,10 @@
 --     age INT,
 --     is_cool BOOLEAN  //verdadeiro ou falso
 -- )
+
+
+-- Exemplo de como verificar dados soltos.
+-- Neste caso a tabela(world_food) ira retornar todos os paises da que Inicaiam com a letra 'U '
+
+-- SELECT country FROM world_food 
+-- WHERE  country LIKE 'U' || '%'   <- Aqui a comparação
